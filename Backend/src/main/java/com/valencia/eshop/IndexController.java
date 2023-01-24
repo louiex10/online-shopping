90d34b747015d2372c54e24e03d31bf097b86f38
Luis Alarcon <lalarcon5@mail.valenciacollege.edu>
2023-01-23 23:34:08 -0500

    error handler

---
+++ Backend/src/main/java/com/valencia/eshop/IndexController.java
@@ -0,0 +1,17 @@
+package com.valencia.eshop;
+
+@RestController
+public class IndexController implements ErrorController{
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
