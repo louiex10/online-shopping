b3238dbef8f4d3a4b67fea6b331ab69539a7a60b
Luis Alarcon <lalarcon5@mail.valenciacollege.edu>
2023-01-24 01:21:07 -0500

    working example

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,20 +1,34 @@
 window.addEventListener('DOMContentLoaded', async function() {
-    console.log("Hello");
-    const custTable = this.document.querySelector("#custTable");
+    // console.log('Hello');
+    // Select table using id
+    const custTable = this.document.querySelector('#custTable');
 
+    // Create a table body as base
     const tbody = custTable.createTBody();
 
-    createRow(tbody, ["1", "Luis Alarcon", "lalarcon5@mail.valenciacollege.edu"]);
-    createRow(tbody, ["2", "John Jenkins", "jjenkins@mail.valenciacollege.edu"]);
-    createRow(tbody, ["3", "Will", "will@mail.valenciacollege.edu"]);
+    // createRow(tbody, ['1', 'Luis Alarcon', 'lalarcon5@mail.valenciacollege.edu']);
+    // createRow(tbody, ['2', 'John Jenkins', 'jjenkins@mail.valenciacollege.edu']);
+    // createRow(tbody, ['3', 'Will', 'will@mail.valenciacollege.edu']);
 
-    var getCustomers = await this.fetch("https://valenciashopping.store/api/customers", { headers: { 'Access-Control-Allow-Origin': '*' } });
-    var cust = getCustomers.json();
-    console.log(cust);
+    // fetch customer data from api
+    const getCustomers = await this.fetch('https://valenciashopping.store/api/customers');
+    const customers = await getCustomers.json();
+
+    // For each customer, add a row to customer table
+    customers.forEach((c) => {
+        createRow(tbody, [c.id, c.name, c.email]);
+    })
 
 });
 
-function createRow(tbody, data = ["1", "2", "3"]) {
+/**
+ * Takes as input a table body and an array of data of any length.
+ * Inserts row into tbody with cells for each piece of data.
+ * @param {HTMLTableSectionElement} tbody Table body
+ * @param {Array.<String>} data Array with data for each cell.
+ * @returns {void} Nothing is returned. tbody is mutated directly.
+ */
+function createRow(tbody, data = ['1', '2', '3']) {
     const trow = tbody.insertRow();
     for (let i = 0; i < data.length; i++) {
         const tc1 = trow.insertCell();
