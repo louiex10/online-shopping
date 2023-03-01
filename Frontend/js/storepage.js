a2bf88b237add594668e17a1ae993a7899209602
rmiller149 <123562904+rmiller149@users.noreply.github.com>
2023-03-01 12:25:41 -0600

    Store Update 1

--- Frontend/js/storepage.js
+++ Frontend/js/storepage.js
@@ -4,13 +4,19 @@ fetch('https://valenciashopping.store/api/products').then((data)=> {
 .then ((completedata) => {
   let products="";
   completedata.map((values)=>{
-    products+=` <div class="listing">
+    products+=`
+    <div class="wrapper">
+         <div class="listing">
+         <br></br>
+         <br></br>
          <h1 class="name">${values.name}</h1>
          <img src=${values.image_url} class="images">
           <p>${values.description}</p>
           <p class="category">${values.category}</p>
-          <p class="price">${values.price}</p>
-        </div>`;
+          <p class="price">$${values.price}</p>
+        </div>
+        </div>
+        `;
       });
       document.getElementById("listings").innerHTML=products;
 
