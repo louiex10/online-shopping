45706876f896e70d550e61eedaaca91f7240c461
louiex10 <lalarcon5@mail.valenciacollege.edu>
2023-04-21 01:05:49 -0400

    add jwt expire logic

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -4,6 +4,28 @@ if (!user) {
     window.location.href = "login.html";
 }
 
+const mytoken = localStorage.getItem("token");
+if (mytoken) {
+    // check if mytoken is expired
+    const exp = parseJwt(mytoken).exp;
+    if (exp) {
+        // check if token is expired
+        const now = new Date().getTime() / 1000;
+        if (now > exp) {
+            logout(true);
+        }
+        // set time to logout
+        const timeToLogout = exp - now;
+        console.log("Time to logout: " + timeToLogout);
+        setTimeout(() => { logout(true) }, timeToLogout * 1000);
+    } else {
+        logout(true);
+    }
+} else {
+    // no token, so logout
+    window.location.href = "login.html";
+}
+
 // -- On Startup --
 window.addEventListener('DOMContentLoaded', async function() {
     console.log("auth.js loaded")
@@ -14,8 +36,23 @@ window.addEventListener('DOMContentLoaded', async function() {
     }
 });
 
-function logout() {
+function logout(expired = false) {
+    console.log("logout");
     localStorage.removeItem("user");
     localStorage.removeItem("token");
-    window.location.href = "login.html";
+    if (!expired) {
+        window.location.href = "login.html";
+    } else {
+        window.location.href = "login.html?expired=true";
+    }
+}
+
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
 }
